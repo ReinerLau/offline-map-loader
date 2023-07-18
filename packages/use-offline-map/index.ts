763fa8f4c86790464e1b86937efc7444fa51a4cb
ReinerLau <lk850593913@gmail.com>
2023-07-18 14:11:05 +0800

    feat(use-offline-map): export component

--- packages/use-offline-map/index.ts
+++
@@ -1,6 +0,0 @@
-export const useOfflineMap = () => {
-  const test = "test";
-  return {
-    test,
-  };
-};
---
+++ packages/use-offline-map/index.tsx
@@ -0,0 +1,6 @@
+export const useOfflineMap = () => {
+  const UseOFFlineMap = () => <div>test</div>;
+  return {
+    UseOFFlineMap,
+  };
+};
