43ded441ec5771f1264edd8ecd4fd8ca6b179c43
ReinerLau <lk850593913@gmail.com>
2023-07-18 14:45:17 +0800

    feat(use-offline-map): init map

--- packages/use-offline-map/index.tsx
+++ packages/use-offline-map/index.tsx
@@ -1,5 +1,25 @@
+import * as maptalks from "maptalks";
+import "maptalks/dist/maptalks.css";
+import { Ref, onMounted, ref } from "vue";
+
 export const useOfflineMap = () => {
-  const UseOFFlineMap = () => <div>test</div>;
+  function initMap() {
+    new maptalks.Map(mapRef.value as HTMLElement, {
+      center: [0, 0],
+      zoom: 2,
+      baseLayer: new maptalks.TileLayer("base", {
+        urlTemplate:
+          "http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
+        subdomains: ["a", "b", "c", "d"],
+        attribution:
+          '&copy; <a href="http://www.osm.org/copyright">OSM</a> contributors, ' +
+          '&copy; <a href="https://carto.com/attributions">CARTO</a>',
+      }),
+    });
+  }
+  onMounted(initMap);
+  const mapRef: Ref<HTMLElement | undefined> = ref();
+  const UseOFFlineMap = () => <div style="height:100%" ref={mapRef}></div>;
   return {
     UseOFFlineMap,
   };
