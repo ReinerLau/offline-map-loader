4beabb894dd5ed190a9010ff4578c0aea051577a
ReinerLau <lk850593913@gmail.com>
2023-07-20 15:54:36 +0800

    feat(use-offline-map): cutsom options

--- examples/normal/src/App.vue
+++ examples/normal/src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { useOfflineMap } from 'use-offline-map'
 
-const { UseOFFlineMap } = useOfflineMap()
+const center = [(113.157014 + 113.69204200000001) / 2, (22.20103800000001 + 22.772601999999978) / 2]
+const { UseOFFlineMap } = useOfflineMap(
+  { center, zoom: 11, maxZoom: 11, minZoom: 11 },
+  { limitExtend: true }
+)
 </script>
 
 <template>
--- packages/use-offline-map/index.tsx
+++ packages/use-offline-map/index.tsx
@@ -2,27 +2,44 @@ import * as maptalks from "maptalks";
 import "maptalks/dist/maptalks.css";
 import { FunctionalComponent, Ref, onMounted, ref } from "vue";
 
-export const useOfflineMap = () => {
+interface Options {
+  limitExtend?: boolean;
+}
+
+export const useOfflineMap = (
+  initMapOption: maptalks.MapOptions,
+  options?: Options
+) => {
+  let map: maptalks.Map;
   function initMap() {
-    new maptalks.Map(mapRef.value as HTMLElement, {
-      center: [
-        (113.157014 + 113.69204200000001) / 2,
-        (22.20103800000001 + 22.772601999999978) / 2,
-      ],
-      zoom: 11,
-      minZoom: 11,
-      maxZoom: 11,
+    map = new maptalks.Map(mapRef.value as HTMLElement, {
       baseLayer: new maptalks.TileLayer("base", {
         urlTemplate: "/tiles/{z}/{x}/{y}.png",
       }),
+      ...initMapOption,
     });
   }
-  onMounted(initMap);
+
+  function getMapInstance() {
+    return map;
+  }
+
+  function handleLimitExtent() {
+    const extent = map.getExtent();
+    map.setMaxExtent(extent);
+  }
+
+  onMounted(() => {
+    initMap();
+    options?.limitExtend && handleLimitExtent();
+  });
+
   const mapRef: Ref<HTMLElement | undefined> = ref();
   const UseOFFlineMap: FunctionalComponent = () => (
     <div style="height:100%" ref={mapRef}></div>
   );
   return {
     UseOFFlineMap,
+    getMapInstance,
   };
 };
